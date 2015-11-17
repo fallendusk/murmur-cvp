800ac4f1043324bb62f57b71047b1177e6016b87
Greg Helton <greg@fallendusk.com>
2015-11-17 18:42:15 -0500

    Enable debugging

--- boot_flaskcvp.sh
+++ boot_flaskcvp.sh
@@ -2,4 +2,4 @@
 
 cd /srv/flaskcvp
 
-python flaskcvp.py -c "Meta:tcp -h $MURMUR_ICE_HOST -p $MURMUR_ICE_PORT" -H 0.0.0.0 
+python flaskcvp.py -c "Meta:tcp -h $MURMUR_ICE_HOST -p $MURMUR_ICE_PORT" -H 0.0.0.0 -d
